e606861c6241f3aaa4ca07d9e2124ad722e8f0b4
Athar Fathana Rakasyah <atharfathana@Athars-MacBook-Air.local>
2025-04-28 13:39:43 +0800

    fix topKfrequent element code

--- Array & Hashing/topKFrequentElement.py
+++ Array & Hashing/topKFrequentElement.py
@@ -1,24 +1,21 @@
 nums = [1,1,1,2,2,3]
 k  = 2
 
-count = {}
-freq = [[] for i in range(len(nums) + 1)]
-# Populate the dictionary
-for n in nums:
-    count[n] = 1 + count.get(n, 0)
-# iterate through count dict and populate the freq list
+def topKfrequentElement(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums)+ 1)]
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+    # count = { 1:3 , 2:2 , 3:1 } 
+    for n, c in count.items():
+        freq[c].append(n)
+    # freq = [[], [3], [2], [1], [], [], []]
+    res = []
+    for i in range(len(freq) - 1, 0, -1): # looping from the last index of the array becuse we want the most frequent element
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+    return []
 
-print(f"{count}\n")
-
-for c,n in count.items():
-    freq[c].append(n)
-
-print(f"{freq}\n")
-res = []
-for i in range(len(nums) - 1, 0, -1):
-    for n in freq[i]:
-        res.append(n)
-    if len(res) == k : print("got em")
-    else: print("Not Found")
-print(f"{res}\n")
-
+print(topKfrequentElement(nums,k))
