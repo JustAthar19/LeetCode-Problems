d9112367b9d800a537dec47673e069f2f445bfe7
JustAthar19 <atharfathana19052003@gmail.com>
2025-10-18 11:07:18 +0700

    test

--- Two Pointers/remove_duplicates.py
+++ Two Pointers/remove_duplicates.py
@@ -1,3 +1,8 @@
+"""
+Problem: Remove Duplicates - Input Array is Sorted
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+Category: Two pointers
+"""
 
 
 nums = [1,1,2,3,3,4]
@@ -7,7 +12,7 @@ def remove_duplicates(nums):
     j = 1
     for i in range(1,len(nums)):
         if nums[i] != nums[i-1]:
-            nums[j] = nums[i]
+            nums[j] = nums[i] # overwrite
             j += 1
             
     return nums
