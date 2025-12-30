144805d35d4dd6bdc9dea6a2b9fd79ee009882dd
JustAthar19 <atharfathana19052003@gmail.com>
2025-12-30 20:34:11 +0800

    add dfs solution to invert binary tree

--- tree/invert_binary_tree.py
+++ tree/invert_binary_tree.py
@@ -5,26 +5,32 @@ Category: Tree
 """
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
-root = [4,2,7,1,3,6,9]
-def inverTree(root):
-    if not root:
-        return None
-    stack = [root]
-    while stack:
-        node = stack.pop()
-        node.left, node.right = node.right, node.left
-        if node.left:
-            stack.append(node.left)
-        if node.right:
-            stack.append(node.right) 
-    return root
+    root = [4,2,7,1,3,6,9]
+    def inverTree(root):
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right) 
+        return root
 
 
-    
+    def invertTreeRecursive(root):
+        if not root:
+            return None
+        
+        root.left, root.right = root.right, root.left
+        self.inverTreeRecursive(root.left)
+        self.invertTreeRecursive(root.right)
